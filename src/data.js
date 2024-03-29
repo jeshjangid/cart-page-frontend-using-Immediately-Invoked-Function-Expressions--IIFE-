a69f557f0bc5f00ff3f3b9ab162d5607297c8051
Rajesh Jangid <149564962+jeshjangid@users.noreply.github.com>
2024-03-29 16:52:03 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopImagesData = [{
+    id: 'asdf',
+    name: 'T-Shirt',
+    price: 25,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: 'img/img1.jpg'
+},{
+    id: 'jkl',
+    name: 'Casual-Shirt',
+    price: 50, 
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: 'img/img3.jpg'
+},{
+    id: 'asdfg',
+    name: 'red-frock',
+    price: 255,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: 'img/img4.jpg'
+},{
+    id: 'hjkl',
+    name: 'women Suit',
+    price: 300,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    img: 'img/img2.jpg'
+}] 
